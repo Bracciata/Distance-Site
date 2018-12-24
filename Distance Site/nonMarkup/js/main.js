8ddffbe12c9b54170fec876f75d09e64b45369ca
Thomas Braccia <bracciata@gmail.com>
2018-12-23 21:35:58 -0600

    Added base vue from https://codepen.io/CSWApps/pen/oWJZqQ
    This is code to autocomplete a field and get the values needed. I made it so there were two but I am unsure of effiency for my purposes. I am going to read up on how to best use it with two fields as I currently have never worked with Vue. Will continue to figure out especially once I have access to a wifi instead of just a hotspot.

--- Distance Site/nonMarkup/js/main.js
+++ Distance Site/nonMarkup/js/main.js
@@ -1,4 +1,36 @@
-function helloWorld() {
-    var x = 'hey';
-    
-}
+
+new Vue({
+    el: '#app',
+    data: {
+      googleMap: '',
+      address: '',
+      lat: '',
+      lng: ''
+    },
+    mounted: function(){
+      this.googleMap = new google.maps.places.Autocomplete((document.getElementById('autocomplete')),{types: ['geocode']});
+      
+      this.googleMap.addListener('place_changed', function() {
+        this.fillInAddress(this.googleMap.getPlace(),0);
+      }.bind(this));
+      this.googleMap2 = new google.maps.places.Autocomplete((document.getElementById('autocomplete2')),{types: ['geocode']});
+      this.googleMap2.addListener('place_changed', function() {
+        this.fillInAddress(this.googleMap2.getPlace(),1);
+      }.bind(this));
+    },
+    methods: {
+      fillInAddress: function(place, locationNumber) {
+          switch(locationNumber){
+              case 0:
+              this.lat = place.geometry.location.lat();
+              this.lng = place.geometry.location.lng();
+              this.address = place.name;
+              break;
+              case 1:
+              break;
+          }
+
+      },
+      canSubmit: function() {}
+    }
+  });
